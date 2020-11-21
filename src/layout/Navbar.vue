3a5178f40b198e3fd38ac08063c5f70ea75d1437
MatthiasChometon <matthias.chometon@ynov.com>
2020-11-21 17:29:36 +0100

    add layout

---
+++ src/layout/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="white"
+        elevate-on-scroll
+        scroll-target="#scrolling-techniques-7"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Ubereat</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-sheet
+        id="scrolling-techniques-7"
+        class="overflow-y-auto"
+        min-height="100vh"
+      >
+        <v-main>
+          <v-container fluid>
+            <router-view></router-view>
+          </v-container>
+        </v-main>
+      </v-sheet>
+    </v-card>
+
+    <v-footer app> </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+
+  components: {},
+
+  data: () => ({}),
+};
+</script>
